f376c7d7a3e9e303687c2dee22123295fe595e5b
EastSun1616 <programmer628@gmail.com>
2021-01-21 15:38:18 +0900

    Logo Loding & SelectActivity Added

--- app/src/main/java/com/inha/incomapp/MainActivity.java
+++ app/src/main/java/com/inha/incomapp/MainActivity.java
@@ -2,7 +2,12 @@ package com.inha.incomapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startLoading();
     }
+
+    private void startLoading() {
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent intent= new Intent(getApplicationContext(), SelectActivity.class);
+                startActivity(intent);  //Loagin화면을 띄운다.
+                finish();   //현재 액티비티 종료
+            }
+        }, 2000); // 화면에 Logo 2초간 보이기
+    }
+
 }
