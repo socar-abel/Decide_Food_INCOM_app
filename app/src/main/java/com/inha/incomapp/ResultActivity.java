63a735bab464ad411b55334f15fbcd421a51b9d8
EastSun1616 <programmer628@gmail.com>
2021-01-21 20:36:22 +0900

    Question and Random added

--- app/src/main/java/com/inha/incomapp/ResultActivity.java
+++ app/src/main/java/com/inha/incomapp/ResultActivity.java
@@ -2,7 +2,10 @@ package com.inha.incomapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 public class ResultActivity extends AppCompatActivity {
 
@@ -10,5 +13,9 @@ public class ResultActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+        Intent intent = getIntent();
+        String[] data = intent.getStringArrayExtra("data");
+        Log.d("DATA",data[0] + ", " + data[1] + ", " + data[2] + ", " + data[3]);
+
     }
 }
